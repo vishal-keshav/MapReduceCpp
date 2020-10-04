b2214ffa4eb7d7646dc242b42f6eb48dc12e19d4
Vishal Keshav <vishal.keshav.1993@gmail.com>
2020-10-04 15:20:00 -0400

    added an adhoch version of map reduce

--- include/Reducer.hpp
+++ include/Reducer.hpp
@@ -9,7 +9,5 @@ class Reducer {
 public:
     ReducerResult execute(std::vector<std::string> tempFileNames,
         std::string outDirectoryName, 
-        std::vector<std::pair<std::string, int>> (*r_fn)(
-            std::string key, std::string value
-        ), int nr_reducer);
+        int (*r_fn)(std::string k, std::vector<int> values), int nr_reducer);
 };
