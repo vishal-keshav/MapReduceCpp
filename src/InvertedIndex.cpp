7ec14695e269c1333ac21f24871a4659058ae897
jessie-huo <perfectvacuum000@gmail.com>
2020-10-09 21:19:46 +0800

    implemented and runned inverted index app

--- src/InvertedIndex.cpp
+++ src/InvertedIndex.cpp
@@ -1,4 +1,7 @@
-#include <iostream>
+/*
+Use map-reduce library to implement the single process version of Inverted Index application. 
+It reads the file /data/input_InvInd.txt and lists all positions of a word in .
+*/
 
 #include <iostream>
 #include <fstream>
@@ -6,7 +9,6 @@
 #include <sstream>
 #include <vector>
 #include <map>
-
 #include <utility>
 
 #include "MapReduceMaster.h"
@@ -14,14 +16,44 @@
 using namespace std;
 
 
-void map_fn(void) {
-    //[TODO]
+// user defined map function
+vector<pair<string, int>> map_fn(string k, string value) { 
+    // k is the number of the line (to_string(record number)), value is the line
+    vector<pair<string, int>> ret;
+
+    istringstream iss(value);
+    do {
+        string word;
+        iss >> word;
+        if(word.size() != 0) {
+            // pair of the word and the line it is in
+            ret.push_back(make_pair(word, stoi(k)));
+        }
+    } while (iss);
+    return ret;
 }
 
-void reduce_fn(void) {
-    //[TODO]
+// user defined reduce function
+vector<int> reduce_fn(string k, vector<int> values) {
+    vector<int> ret;
+    for (int i = 0; i < sizeof(values); i++){
+        if (find(ret.begin(), ret.end(), values[i]) == ret.end())
+            // values[i] is a new element (position of the word)
+            ret.push_back(values[i]);
+    }
+    return ret;
 }
 
 int main() {
-    // [TODO]
+    // Create master instance.
+    MapReduceMaster<string, string, string, int, int> masterInstance("input.txt", ".", map_fn, reduce_fn);
+    int result = masterInstance.process();
+
+    // Now interpred the result of MapReduce
+    if (result == -1){
+        cout << "InvertedIndex MapReduce failed" << endl;
+    } else {
+        cout << "InvertedIndex MapReduce success" << endl;
+    }
+    return 0;
 }
